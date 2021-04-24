7310973f3bdbd2f022517f88f0331ae65de73fbc
yomex4life <yomex4life2001@gmail.com>
2021-04-24 17:57:34 +0100

    dockerisation complete. just before building the CRUD application

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config');
 
 const app = express();
+//mongoose.connect("mongodb://admin:password@mongodb:27017/?authsource=admin")
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authsource=admin`;
+
+const connectWithRetry = () =>{
+    mongoose.connect(mongoURL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    });
+}
+connectWithRetry();
+
 
 const port = process.env.PORT || 3000;
 
